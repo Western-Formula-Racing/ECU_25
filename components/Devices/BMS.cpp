2f39b18e34852c88fb785b9ea0349bfae838aada
yamaan93 <yamaan93@gmail.com>
2024-11-23 15:20:04 -0500

    start implementing singleton bms class

--- components/Devices/BMS.cpp
+++ components/Devices/BMS.cpp
@@ -1,3 +1,16 @@
 #include "BMS.h"
 #include "esp_log.h"
 static const char* TAG = "BMS"; //Used for ESP_LOGx commands. See ESP-IDF Documentation
+
+BMS* BMS::instancePtr = nullptr;
+SemaphoreHandle_t BMS::mutex = xSemaphoreCreateMutex();
+
+BMS::BMS(){
+    ESP_LOGI(TAG, "BMS Initialized");
+}
+
+BMS* BMS::Get(){
+    if(instancePtr == nullptr){
+    
+    }
+}
---
+++ components/Devices/Inverter.h
@@ -0,0 +1,25 @@
+#ifndef _INVERTER_
+#define _INVERTER_
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+
+
+class Inverter{
+private:
+    //Singleton device class structure
+    static Inverter* instancePtr;
+    static SemaphoreHandle_t mutex;
+    Inverter();
+public:
+    //Deleting the copy constructor and copy reference constructor to prevent copies
+    Inverter(const Inverter &) = delete;
+    Inverter &operator=(const Inverter &) = delete;
+    Inverter(Inverter &&) = delete;
+    Inverter &operator=(Inverter &&) = delete;
+    static Inverter* Get();
+
+};
+
+#endif
