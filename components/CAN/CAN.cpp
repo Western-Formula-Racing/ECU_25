01c0afa92d3e3334472484af5d93f269dd9eba2a
yamaan93 <yamaan93@gmail.com>
2024-12-26 16:10:30 -0500

    setup TWAI

--- components/CAN/CAN.cpp
+++ components/CAN/CAN.cpp
@@ -6,3 +6,26 @@ int give_zero(){
     printf("kill me please I can't keep doing this\n");
     return 0;
 }
+
+
+
+
+CAN::CAN(gpio_num_t CAN_TX_Pin, gpio_num_t CAN_RX_Pin, twai_mode_t twai_mode)
+{
+    twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(CAN_TX_Pin, CAN_RX_Pin, twai_mode);
+    twai_timing_config_t t_config = TWAI_TIMING_CONFIG_500KBITS();
+    twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
+    g_config.controller_id = 0;
+    if (twai_driver_install_v2(&g_config, &t_config, &f_config, &twai_handle) == ESP_OK) {
+        printf("Driver installed\n");
+    } else {
+        printf("Failed to install driver\n");
+        return;
+    }
+    if (twai_start_v2(twai_handle) == ESP_OK) {
+        printf("Driver started\n");
+    } else {
+        printf("Failed to start driver\n");
+        return;
+    }
+}
--- components/CAN/CAN.h
+++ components/CAN/CAN.h
@@ -2,21 +2,26 @@
 #define __CAN__
 
 #include <map>
-
-git pu
-
-
-
+#include "driver/twai.h"
+#include "etl/vector.h"
 
 class CAN
 {
 
 public:
-    CAN(int CAN_RX_Pin, int CAN_TX_Pin);
+    CAN(gpio_num_t CAN_TX_Pin, gpio_num_t CAN_RX_Pin, twai_mode_t twai_mode);
     
     
 private:
+    twai_handle_t twai_handle;
+    twai_general_config_t g_config;
+    twai_timing_config_t t_config;
+    twai_filter_config_t f_config;
 
+    void rx_task();
+    void tx_task();
+
+    
 
 
 
