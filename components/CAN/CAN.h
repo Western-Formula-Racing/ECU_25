0e69530a434979aaac78efdfcc31391f64770bca
yamaan93 <yamaan93@gmail.com>
2024-12-26 14:45:04 -0500

    start new branch for CAN

--- components/CAN/CAN.h
+++ components/CAN/CAN.h
@@ -1,1 +1,29 @@
+#ifndef __CAN__
+#define __CAN__
+
+#include <map>
+
+git pu
+
+
+
+
+class CAN
+{
+
+public:
+    CAN(int CAN_RX_Pin, int CAN_TX_Pin);
+    
+    
+private:
+
+
+
+
+};
+
+
+
 int give_zero();
+
+#endif
