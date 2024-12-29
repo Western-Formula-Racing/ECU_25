ad0eab4e1ae76ca7e267d5d919561cbcf7b6213f
yamaan93 <yamaan93@gmail.com>
2024-12-29 18:01:46 -0500

    CAN Rx task blocking all other tasks for some reason

--- components/CAN/CAN.cpp
+++ components/CAN/CAN.cpp
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include "esp_log.h"
 static const char* TAG = "CAN"; //Used for ESP_LOGx commands. See ESP-IDF Documentation
+static SemaphoreHandle_t rx_sem = xSemaphoreCreateBinary();
+
+
 int give_zero(){
     printf("kill me please I can't keep doing this\n");
     return 0;
@@ -13,6 +16,7 @@ int give_zero(){
 CAN::CAN(gpio_num_t CAN_TX_Pin, gpio_num_t CAN_RX_Pin, twai_mode_t twai_mode)
 {
     twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(CAN_TX_Pin, CAN_RX_Pin, twai_mode);
+    g_config.rx_queue_len = 10000;
     twai_timing_config_t t_config = TWAI_TIMING_CONFIG_500KBITS();
     twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
     g_config.controller_id = 0;
@@ -28,8 +32,15 @@ CAN::CAN(gpio_num_t CAN_TX_Pin, gpio_num_t CAN_RX_Pin, twai_mode_t twai_mode)
         ESP_LOGI(TAG,"Failed to start driver\n");
         return;
     }
+    twai_reconfigure_alerts_v2(twai_handle, TWAI_ALERT_RX_QUEUE_FULL, NULL);
+
+}
+
+void CAN::begin()
+{
+    xSemaphoreGive(rx_sem); // you have to give the semaphore on startup lol
     // Create the task
-    xTaskCreatePinnedToCore(CAN::rx_task_wrapper, "CAN_rx", 4096, this, configMAX_PRIORITIES - 1, nullptr, tskNO_AFFINITY);
+    xTaskCreatePinnedToCore(CAN::rx_task_wrapper, "CAN_rx", 4096, this, configMAX_PRIORITIES - 1, nullptr, 1);
 }
 
 void CAN::rx_task_wrapper(void *arg)
@@ -40,14 +51,26 @@ void CAN::rx_task_wrapper(void *arg)
 
 void CAN::rx_task()
 {
-    // xSemaphoreTake(rx_sem, portMAX_DELAY);
-
+    
     while (true) {
-        twai_message_t rx_msg;
-        twai_receive(&rx_msg, portMAX_DELAY);
-        ESP_LOGI(TAG, "Received message with ID %" PRIu32, rx_msg.identifier);
-        for (int i = 0; i < rx_msg.data_length_code; i++) {
-             ESP_LOGI(TAG, "  %d" , rx_msg.data[i]);
+        // Try to take the semaphore without blocking
+        if (xSemaphoreTake(rx_sem, 0) == pdTRUE) {
+            twai_message_t rx_msg;
+            twai_read_alerts_v2(twai_handle, &twai_alerts, 0);
+            if(twai_alerts & TWAI_ALERT_RX_QUEUE_FULL){
+                    ESP_LOGW(TAG, "rx queue full: msg dropped");
+                }
+            if (twai_receive(&rx_msg, 0) == ESP_OK) {
+                ESP_LOGI(TAG, "Rx'd ID %" PRIu32, rx_msg.identifier);
+                // for (int i = 0; i < rx_msg.data_length_code; i++) {
+                //     ESP_LOGI(TAG, "  %d", rx_msg.data[i]);
+                // }
+            }
+            xSemaphoreGive(rx_sem);
+        } else {
+            // Could not get semaphore, yield
+            taskYIELD();
         }
+        vTaskDelay(pdMS_TO_TICKS(10));
     }
 }
--- components/CAN/CAN.h
+++ components/CAN/CAN.h
@@ -11,17 +11,18 @@ class CAN
 {
 public:
     CAN(gpio_num_t CAN_TX_Pin, gpio_num_t CAN_RX_Pin, twai_mode_t twai_mode);
-    static void rx_task_wrapper(void *arg); // Wrapper function
-    void rx_task(); // Non-static member function
+    void begin();
 
 private:
     twai_handle_t twai_handle;
     twai_general_config_t g_config;
     twai_timing_config_t t_config;
     twai_filter_config_t f_config;
-    static SemaphoreHandle_t rx_sem;
+    uint32_t twai_alerts;
 
+    static void rx_task_wrapper(void *arg); // Wrapper function
     void tx_task();
+    void rx_task(); // Non-static member function
 };
 
 int give_zero();
