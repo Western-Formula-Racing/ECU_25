41801efe5e7460f191f93c709d4094babb6e280b
yamaan93 <yamaan93@gmail.com>
2025-01-10 20:05:20 -0500

    apparently timers don't like to be started before main is called?, causes a segfault

--- components/CAN/CAN_Config.hpp
+++ components/CAN/CAN_Config.hpp
@@ -3,9 +3,10 @@
 #include "CAN_Signal.hpp"
 #include "etl/map.h"
 #include "etl/vector.h"
+#include "etl/set.h"
 
 // Defining the CAN_IDs & Signals
-#define INV_CommandMessage 192
+#define INV_M192_Command_Message 192
 inline CAN_Signal torqueRequest_Signal{true, 0, 16, FLOAT, 0.1f};
 inline CAN_Signal directionCommand_Signal{true, 32, 1};
 inline CAN_Signal inverterEnable_Signal{true, 40, 1};
@@ -13,10 +14,17 @@ inline CAN_Signal inverterDischarge_Signal{true, 41, 1};
 inline CAN_Signal speedModeEnable_Signal{true, 42, 1};
 inline CAN_Signal torqueLimit_Signal{true, 48, 16};
 
+#define INV_M176_Fast_Info 176
+inline CAN_Signal INV_Fast_DC_Bus_Voltage{true, 48, 16, FLOAT, 0.1f};
+inline CAN_Signal INV_Fast_Motor_Speed{true, 32, 16};
+inline CAN_Signal INV_Fast_Torque_Command{true, 0, 16, FLOAT, 0.1f};
+inline CAN_Signal INV_Fast_Torque_Feedback{true, 16, 16, FLOAT, 0.1f};
+
 //define the CAN Map here
+//all entries in the map must have the exact same types which means all the vectors have to be the same size
 inline etl::map CAN_Map
 { 
-    etl::pair{INV_CommandMessage, etl::vector<CAN_Signal*, 6>{ 
+    etl::pair{INV_M192_Command_Message, etl::vector<CAN_Signal*, 8>{ 
         &torqueRequest_Signal,
         &directionCommand_Signal,
         &inverterEnable_Signal,
@@ -24,10 +32,18 @@ inline etl::map CAN_Map
         &speedModeEnable_Signal,
         &torqueLimit_Signal
         }},
-
+    etl::pair{INV_M176_Fast_Info, etl::vector<CAN_Signal*, 8>{ 
+        &INV_Fast_DC_Bus_Voltage,
+        &INV_Fast_Motor_Speed,
+        &INV_Fast_Torque_Command,
+        &INV_Fast_Torque_Feedback,
+        }},
 };
 
-
-
+inline etl::set CAN_Rx_IDs = {176, 192};
+inline etl::set CAN_Tx_10ms_IDs = {192};
+// inline etl::set CAN_Tx_100ms_IDs = {};
+// inline etl::set CAN_Tx_1000ms_IDs = {};
+//they don't like to be empty 
 
 #endif 
--- main/main.cpp
+++ main/main.cpp
@@ -19,17 +19,18 @@ CAN can0{GPIO_NUM_47, GPIO_NUM_48, TWAI_MODE_NORMAL};
 // must extern C since this gets called by a C file for freeRTOS
 extern "C" void app_main(void)
 {
-    vTaskDelay(pdMS_TO_TICKS(5000));
+    // vTaskDelay(pdMS_TO_TICKS(5000));
     ESP_LOGI(TAG, "Main Has begun");
     can0.begin();
     
 
     while(true){
-        printf("inverter torque req raw: %lld\n", torqueRequest_Signal.get_raw());
-        printf("inverter torque req uint: %lld\n", torqueRequest_Signal.get_uint64());
-        printf("inverter torque req int: %d\n", torqueRequest_Signal.get_int());
-        printf("inverter torque req bool: %d\n", torqueRequest_Signal.get_bool());
-        printf("inverter torque req float: %f\n", torqueRequest_Signal.get_float());
+        // printf("inverter torque req raw: %lld\n", torqueRequest_Signal.get_raw());
+        // printf("inverter torque req uint: %lld\n", torqueRequest_Signal.get_uint64());
+        // printf("inverter torque req int: %d\n", torqueRequest_Signal.get_int());
+        // printf("inverter torque req bool: %d\n", torqueRequest_Signal.get_bool());
+        // printf("inverter torque req float: %f\n", torqueRequest_Signal.get_float());
+        printf("heart beat!\n");
         vTaskDelay(100);
     }
     
