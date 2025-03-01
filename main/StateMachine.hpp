306e1302dffcd4dbd314e6e8e98e7bee87a64978
yamaan93 <yamaan93@gmail.com>
2025-03-01 08:09:34 -0500

    create state machine structure

---
+++ main/StateMachine.hpp
@@ -0,0 +1,32 @@
+#ifndef __STATE_MACHINE__
+#define __STATE_MACHINE__
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "etl/array.h"
+
+namespace StateMachine{
+enum State
+{
+    START,
+    PRECHARGE_ENABLE,
+    PRECHARGE_OK,
+    STARTUP_DELAY,
+    DRIVE,
+    PRECHARGE_ERROR,
+    DEVICE_FAULT,
+};
+// Creating a type for state function pointers
+typedef State (*state_function_t)();
+
+
+void StateMachineLoop(void *);
+State handle_start();
+State handle_precharge_enable();
+State handle_precharge_ok();
+State handle_startup_delay();
+State handle_drive();
+State handle_precharge_error();
+State handle_device_fault();
+
+}
+#endif
