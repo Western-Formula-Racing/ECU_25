3ceb44ebb0d5f40a62577c38b8b9d97aeabeb8a5
faris-amer <farisamer569@gmail.com>
2025-03-19 07:17:49 -0400

    first working adc conversion

--- components/IO/IO.h
+++ components/IO/IO.h
@@ -4,6 +4,11 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/semphr.h"
+#include "driver/spi_master.h"
+#include "PinMap.h"
+
+#define ADC_CMD_READ    0b00010000
+#define ADC_CMD_WRITE   0b00001000
 
 
 class IO{
@@ -11,15 +16,21 @@ private:
     //Singleton device class structure
     static IO* instancePtr;
     static SemaphoreHandle_t mutex;
-    IO();
-public:
+    spi_device_handle_t adc1Handle;
+    spi_device_handle_t adc2Handle;
+  public:
     //Deleting the copy constructor and copy reference constructor to prevent copies
+    IO();
     IO(const IO &) = delete;
     IO &operator=(const IO &) = delete;
     IO(IO &&) = delete;
     IO &operator=(IO &&) = delete;
     static IO* Get();
+    void Setup();
 
 };
 
+void io_task(void *pvParameters);
+void adcSetup(spi_device_handle_t devHandle);
+void adcRead(spi_device_handle_t devHandle, uint8_t channel ,uint8_t *rx_buffer);
 #endif
