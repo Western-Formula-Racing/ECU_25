3412516cc35169c8faa377bd38d64b6aba00a566
faris-amer <farisamer569@gmail.com>
2025-03-20 22:17:59 -0400

    cleaned up previous commmits

--- components/IO/IO.h
+++ components/IO/IO.h
@@ -12,13 +12,13 @@
 class IO{
 private:
     //Singleton device class structure
+    IO();
     static IO* instancePtr;
     static SemaphoreHandle_t mutex;
     spi_device_handle_t adc1Handle;
     spi_device_handle_t adc2Handle;
   public:
     //Deleting the copy constructor and copy reference constructor to prevent copies
-    IO();
     IO(const IO &) = delete;
     IO &operator=(const IO &) = delete;
     IO(IO &&) = delete;
