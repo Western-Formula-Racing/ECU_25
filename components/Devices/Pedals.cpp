d2df021769a4b707099767ed63ba54ee51681f1c
yamaan93 <yamaan93@gmail.com>
2025-04-01 15:10:52 -0400

    add basic pedal functionality

--- components/Devices/Pedals.cpp
+++ components/Devices/Pedals.cpp
@@ -26,3 +26,46 @@ Pedals *Pedals::Get()
     }
     return instancePtr;
 }
+
+float Pedals::getThrottle()
+{
+    throttle = 0;
+    // Temporary test values
+    apps1_min_voltage = 0.0f;
+    apps2_min_voltage = 0.0f;
+    apps1_max_voltage = 2.0f;
+    apps2_max_voltage = 2.0f;
+    float apps1_voltage = apps1_voltage_signal.get_float();
+    float apps2_voltage = apps2_voltage_signal.get_float();
+    
+    apps_sensor1_percent = min(1, max(0, (apps1_voltage - apps1_min_voltage) / (apps1_max_voltage - apps1_min_voltage)));
+    apps_sensor2_percent = min(1, max(0, (apps2_voltage - apps2_min_voltage) / (apps2_max_voltage - apps2_min_voltage)));
+    throttle = (apps_sensor1_percent + apps_sensor2_percent)/2;
+    // Case 1: sensor open circuit
+    if(apps1_voltage <= 0.01f || apps2_voltage <= 0.01f)
+    {
+        fault_latch = true;
+        throttle = 0;
+    }
+
+    // Case 2: pedal implausability
+    else if(max(apps_sensor1_percent, apps_sensor2_percent) - min(apps_sensor1_percent, apps_sensor2_percent))
+    {
+        fault_latch = true;
+        throttle = 0;
+    }
+
+    // Case 3: clear faults
+    else if(throttle <= 0.05f)
+    {
+        fault_latch = false;
+    }
+
+
+    if(fault_latch){
+        throttle = 0;
+    }
+
+    return throttle;
+
+}
