0ebce187a2d6580bb6a04623c82e765879578274
Blankk <williams.edi012@gmail.com>
2025-04-23 04:41:47 -0400

    Now used method to get buffer

--- components/GUI/FloatRecievable.cpp
+++ components/GUI/FloatRecievable.cpp
@@ -7,8 +7,7 @@ FloatRecievable::FloatRecievable(float default_value):Recievable<float>(default_
 
 char* FloatRecievable::serialize_to_json()
 {
-    char buffer[RECV_BUFFER_SIZE];
-    memset(buffer, 0, RECV_BUFFER_SIZE);
-    snprintf(buffer, RECV_BUFFER_SIZE-1, "{\"value\":%.5f, \"type\":\"recievable>float\"}", get_value());
+    char* buffer = allocate_buffer();
+    snprintf(buffer, RECV_BUFFER_SIZE-1, "{\"value\":%.5f, \"type\":\"recievable>float\"}", value);
     return buffer;
 }
