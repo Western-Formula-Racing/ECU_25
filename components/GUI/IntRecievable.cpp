8fee6f1ff3964701f345bde9eaba6274ee769115
Blankk <williams.edi012@gmail.com>
2025-04-23 04:44:26 -0400

    Now uses allocate_buffer() to get buffer for serialization

--- components/GUI/IntRecievable.cpp
+++ components/GUI/IntRecievable.cpp
@@ -8,8 +8,7 @@ IntRecievable::IntRecievable(int default_value):Recievable<int>(default_value)
 
 char *IntRecievable::serialize_to_json(void)
 {
-    char buffer[RECV_BUFFER_SIZE];
-    memset(buffer, 0, RECV_BUFFER_SIZE);
-    snprintf(buffer, RECV_BUFFER_SIZE-1, "{\"value\":%d, \"type\":\"recievable>int\"}", get_value());
+    char* buffer = allocate_buffer();
+    snprintf(buffer, RECV_BUFFER_SIZE-1, "{\"value\":%d, \"type\":\"recievable>int\"}", value);
     return buffer;
 }
--- components/GUI/StringRecievable.cpp
+++ components/GUI/StringRecievable.cpp
@@ -8,8 +8,7 @@ StringRecievable::StringRecievable(char* default_value):Recievable<char*>(defaul
 
 char *StringRecievable::serialize_to_json(void)
 {
-    char buffer[RECV_BUFFER_SIZE];
-    memset(buffer, 0, RECV_BUFFER_SIZE);
-    snprintf(buffer, RECV_BUFFER_SIZE-1, "{\"value\":\"%s\", \"type\":\"recievable>string\"}", get_value());
+    char* buffer = allocate_buffer();
+    snprintf(buffer, RECV_BUFFER_SIZE-1, "{\"value\":\"%s\", \"type\":\"recievable>string\"}", value);
     return buffer;
 }
