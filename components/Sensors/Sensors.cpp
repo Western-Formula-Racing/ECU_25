081b26a64ba846a792a8732867d2f0383edf852e
Haorui Zhou <haorui2002@gmail.com>
2026-04-19 23:04:56 -0400

    Pitot tube message 2030, tested ok need calib

--- components/Sensors/Sensors.cpp
+++ components/Sensors/Sensors.cpp
@@ -1,5 +1,6 @@
 #include "Sensors.h"
 #include "esp_log.h"
+#include <cmath>
 static const char *TAG = "Sensors"; // Used for ESP_LOGx commands. See ESP-IDF Documentation
 
 Sensors *Sensors::instancePtr = nullptr;
@@ -47,6 +48,15 @@ void Sensors::poll_sensors()
     A12_ID2016.set(Sensors::Get()->get_sensor_voltage(static_cast<Sensors::SENSOR_INDEX>(11)));
     A13_ID2017.set(Sensors::Get()->get_sensor_voltage(static_cast<Sensors::SENSOR_INDEX>(12)));
     A14_ID2017.set(Sensors::Get()->get_sensor_voltage(static_cast<Sensors::SENSOR_INDEX>(13)));
+    float pitot_voltage = Sensors::Get()->get_sensor_voltage(Sensors::PITOT);
+    float pitot_pressure_mbar = (pitot_voltage - 0.5f) * 6.25f + 50.0f;
+    float pitot_dynamic_mbar = pitot_pressure_mbar - 50.0f;
+    float pitot_speed_kph = (pitot_dynamic_mbar > 0.0f) ? sqrtf(2.0f * pitot_dynamic_mbar * 100.0f / 1.225f) * 3.6f : 0.0f;
+    printf(">Pitot_Pressure:%.2f\n", pitot_pressure_mbar);
+    printf(">Pitot_Speed:%.2f\n", pitot_speed_kph);
+    Pitot_Voltage_ID2030.set(pitot_voltage);
+    Pitot_Pressure_ID2030.set(pitot_pressure_mbar);
+    Pitot_Speed_ID2030.set(pitot_speed_kph);
 }
 
 etl::array<float,14> Sensors::get_sensor_voltages(){
